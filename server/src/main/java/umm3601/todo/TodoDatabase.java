afb5c78be5f724d8691c674a23ed094b6cae00d5
Brendan Conroy <conro188@morris.umn.edu>
2023-01-26 17:12:41 -0600

    Done with all epics. just have testing left to do

--- server/src/main/java/umm3601/todo/TodoDatabase.java
+++ server/src/main/java/umm3601/todo/TodoDatabase.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.javalin.http.BadRequestResponse;
+//import io.javalin.http.BadRequestResponse;
 
 /**
  * A fake "database" of user info
@@ -18,12 +18,12 @@ import io.javalin.http.BadRequestResponse;
  * then provide various database-like methods that allow the `UserController` to
  * "query" the "database".
  */
-public class TodoDatabase{
+public class TodoDatabase {
 
   private Todo[] allTodos;
 
-  public TodoDatabase(String TodoDataFile) throws IOException {
-    InputStreamReader reader = new InputStreamReader(getClass().getResourceAsStream(TodoDataFile));
+  public TodoDatabase(String todoDataFile) throws IOException {
+    InputStreamReader reader = new InputStreamReader(getClass().getResourceAsStream(todoDataFile));
     ObjectMapper objectMapper = new ObjectMapper();
     allTodos = objectMapper.readValue(reader, Todo[].class);
   }
@@ -52,91 +52,76 @@ public class TodoDatabase{
   public Todo[] listTodos(Map<String, List<String>> queryParams) {
     Todo[] filteredTodos = allTodos;
 
-    //   // Filter status if defined
-    //  if (queryParams.containsKey("status")) {
-    //    Boolean statusParam = queryParams.get("status").get(0);
-    //    try {
-    //      int targetStatus = Boolean.parseBoolean(statusParam);
-    //      filteredTodos = filterTodosByStatus(filteredTodos, targetStatus);
-    //    } catch (BooleanFormatException e) {
-    //      throw new BadRequestResponse("Specified age '" + statusParam + "' can't be parsed to an integer");
-    //    }
-    //  }
-
-      // Filter owner if defined
-      if (queryParams.containsKey("owner")) {
-        String targetOwner = queryParams.get("owner").get(0);
-        filteredTodos = filterTodosByOwner(filteredTodos, targetOwner);
+    // Filter owner if defined
+    if (queryParams.containsKey("owner")) {
+      String targetOwner = queryParams.get("owner").get(0);
+      filteredTodos = filterTodosByOwner(filteredTodos, targetOwner);
+    }
+    // Process other query parameters here...
+    // Filter category if defined
+    if (queryParams.containsKey("category")) {
+      String targetCategory = queryParams.get("category").get(0);
+      filteredTodos = filterTodosByCategory(filteredTodos, targetCategory);
+    }
+    // Support searching for bodies containing given string
+    if (queryParams.containsKey("contains")) {
+      String targetContains = queryParams.get("contains").get(0);
+      filteredTodos = filterTodosByContains(filteredTodos, targetContains);
+    }
+    // Filter status
+    if (queryParams.containsKey("status")) {
+      String targetStatusString = queryParams.get("status").get(0);
+      Boolean targetStatus = false;
+      if (targetStatusString.equals("complete")) {
+        targetStatus = true;
       }
-      // Process other query parameters here...
-
-        // Filter category if defined
-        if (queryParams.containsKey("category")) {
-          String targetCategory = queryParams.get("category").get(0);
-          filteredTodos = filterTodosByCategory(filteredTodos, targetCategory);
-        }
-        // Process other query parameters here...
-
-        // // limit number of todos displayed
-
-        if (queryParams.containsKey("limit")) {
-          String targetLimit = queryParams.get("limit").get(0);
-          filteredTodos = filterTodosByLimit(filteredTodos, targetLimit);
-        }
-
-          // Support searching for bodies containing given string
-          if (queryParams.containsKey("contains")) {
-            String targetContains = queryParams.get("contains").get(0);
-            filteredTodos = filterTodosByContains(filteredTodos, targetContains);
-          }
-
-          // support ordering/sorting of todos by attribute
-
-          if (queryParams.containsKey("orderBy")){
-            String targetOrder = queryParams.get("orderBy").get(0);
-            filteredTodos = filterTodosByOrder(filteredTodos, targetOrder);
-          }
-
-
-      return filteredTodos;
+      filteredTodos = filterTodosByStatus(filteredTodos, targetStatus);
     }
 
-
-    public Todo[] filterTodosByOwner(Todo[] todos, String targetOwner) {
-      return Arrays.stream(todos).filter(x -> x.owner.equals(targetOwner)).toArray(Todo[]::new);
+    // support ordering/sorting of todos by attribute
+    if (queryParams.containsKey("orderBy")) {
+      String targetOrder = queryParams.get("orderBy").get(0);
+      filteredTodos = filterTodosByOrder(filteredTodos, targetOrder);
     }
 
-    public Todo[] filterTodosByCategory(Todo[] todos, String targetCategory) {
-      return Arrays.stream(todos).filter(x -> x.category.equals(targetCategory)).toArray(Todo[]::new);
+    // limit number of todos displayed
+    if (queryParams.containsKey("limit")) {
+      String targetLimit = queryParams.get("limit").get(0);
+      filteredTodos = filterTodosByLimit(filteredTodos, targetLimit);
     }
+    return filteredTodos;
+  }
 
-    public Todo[] filterTodosByContains(Todo[] todos, String targetContains) {
-      return Arrays.stream(todos).filter(x -> x.body.contains(targetContains)).toArray(Todo[]::new);
+  public Todo[] filterTodosByStatus(Todo[] todos, Boolean targetStatus) {
+    return Arrays.stream(todos).filter(x -> x.status.equals(targetStatus)).toArray(Todo[]::new);
+  }
+
+  public Todo[] filterTodosByOwner(Todo[] todos, String targetOwner) {
+    return Arrays.stream(todos).filter(x -> x.owner.equals(targetOwner)).toArray(Todo[]::new);
+  }
+
+  public Todo[] filterTodosByCategory(Todo[] todos, String targetCategory) {
+    return Arrays.stream(todos).filter(x -> x.category.equals(targetCategory)).toArray(Todo[]::new);
+  }
+
+  public Todo[] filterTodosByContains(Todo[] todos, String targetContains) {
+    return Arrays.stream(todos).filter(x -> x.body.contains(targetContains)).toArray(Todo[]::new);
+  }
+
+  public Todo[] filterTodosByLimit(Todo[] todos, String targetLimit) {
+    return Arrays.stream(todos).limit(Integer.valueOf(targetLimit)).toArray(Todo[]::new);
+  }
+
+  public Todo[] filterTodosByOrder(Todo[] todos, String targetOrder) {
+    if (targetOrder.equals("owner")) {
+      return Arrays.stream(todos).sorted(Comparator.comparing((Todo t) -> t.owner)).toArray(Todo[]::new);
+    } else if (targetOrder.equals("body")) {
+      return Arrays.stream(todos).sorted(Comparator.comparing((Todo t) -> t.body)).toArray(Todo[]::new);
+    } else if (targetOrder.equals("status")) {
+      return Arrays.stream(todos).sorted(Comparator.comparing((Todo t) -> t.status)).toArray(Todo[]::new);
+    } else {
+      return Arrays.stream(todos).sorted(Comparator.comparing((Todo t) -> t.category)).toArray(Todo[]::new);
     }
-
-    public Todo[] filterTodosByLimit(Todo[] todos, String targetLimit) {
-      return Arrays.stream(todos).limit(Integer.valueOf(targetLimit)).toArray(Todo[]::new);
-    }
-
-    public Todo[] filterTodosByOrder(Todo[] todos, String targetOrder) {
-      if (targetOrder.equals("owner")){
-
-        return Arrays.stream(todos).sorted(Comparator.comparing((Todo t) -> t.owner)).toArray(Todo[]::new);
-      }
-      else if (targetOrder.equals("body")){
-        return Arrays.stream(todos).sorted(Comparator.comparing((Todo t) -> t.body)).toArray(Todo[]::new);
-
-      }
-      else if (targetOrder.equals("status")){
-        return Arrays.stream(todos).sorted(Comparator.comparing((Todo t) -> t.status)).toArray(Todo[]::new);
-      }
-      else {
-        return Arrays.stream(todos).sorted(Comparator.comparing((Todo t) -> t.category)).toArray(Todo[]::new);
-      }
-    }
-
-
-
-
+  }
 
 }
