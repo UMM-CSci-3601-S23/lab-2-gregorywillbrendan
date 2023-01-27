81a2331f8971ea18437a74cb868bfbeba8cf4f51
Gregory Peterson <pet03356@morris.umn.edu>
2023-01-27 16:17:36 -0600

    finished with testing for todos and with lab

--- server/src/test/java/umm3601/todo/TodoControllerSpec.java
+++ server/src/test/java/umm3601/todo/TodoControllerSpec.java
@@ -1,6 +1,7 @@
 package umm3601.todo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -12,17 +13,19 @@ import java.util.List;
 import java.util.Map;
 
 
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 
 import io.javalin.http.Context;
-
-
+import io.javalin.http.HttpStatus;
+import io.javalin.http.NotFoundResponse;
 import umm3601.Server;
 
 /**
- * Tests the logic of the UserController
+ * Tests the logic of the TodoController
  *
  * @throws IOException
  */
@@ -33,6 +36,7 @@ import umm3601.Server;
 // these tests can/should be restructured so the constants (there are
 // also a lot of "magic strings" that Checkstyle doesn't actually
 // flag as a problem) make more sense.
+
 @SuppressWarnings({ "MagicNumber" })
 public class TodoControllerSpec {
 
@@ -54,12 +58,9 @@ public class TodoControllerSpec {
    */
   @Test
   public void canGetAllTodos() throws IOException {
-    // Call the method on the mock context, which doesn't
-    // include any filters, so we should get all the todos
-    // back.
     todoController.getTodos(ctx);
 
-    // Confirm that `json` was called with all the users.
+    // Confirm that `json` was called with all the todos.
     ArgumentCaptor<Todo[]> argument = ArgumentCaptor.forClass(Todo[].class);
     verify(ctx).json(argument.capture());
     assertEquals(db.size(), argument.getValue().length);
@@ -87,22 +88,24 @@ public class TodoControllerSpec {
   }
   @Test
   public void canGetTodosWithCategoryHomework() throws IOException {
-    // this tests that filtering by category
+    // this tests that filtering by category works
     Map<String, List<String>> queryParams = new HashMap<>();
     queryParams.put("category", Arrays.asList(new String[] {"homework"}));
     when(ctx.queryParamMap()).thenReturn(queryParams);
 
     // Call the method on the mock controller with the added
     // query param map to limit the result to just todos with
-    // owner "Fry".
+    // category "homework"
     todoController.getTodos(ctx);
 
-    // Confirm that all the todos passed to `json` have owner "fry".
+    // Confirm that all the todos passed to `json` have category "homework".
     ArgumentCaptor<Todo[]> argument = ArgumentCaptor.forClass(Todo[].class);
     verify(ctx).json(argument.capture());
     for (Todo todo : argument.getValue()) {
       assertEquals("homework", todo.category);
     }
+
+
   }
 
 
@@ -125,4 +128,118 @@ public class TodoControllerSpec {
     }
     assertEquals(17, argument.getValue().length);
   }
-}
+
+@Test
+
+  public void canGetTodoWithSpecifiedId() throws IOException {
+    String id = "58895985a22c04e761776d54";
+    Todo todo = db.getTodo(id);
+
+    when(ctx.pathParam("id")).thenReturn(id);
+
+    todoController.getTodo(ctx);
+
+    verify(ctx).json(todo);
+    verify(ctx).status(HttpStatus.OK);
+    assertEquals("Blanche", todo.owner);
+  }
+
+@Test
+  public void canGetTodosWithBody() throws IOException {
+    // this tests that filtering by Body works
+    Map<String, List<String>> queryParams = new HashMap<>();
+    queryParams.put("contains", Arrays.asList(new String[] {"tempor"}));
+    when(ctx.queryParamMap()).thenReturn(queryParams);
+
+    // Call the method on the mock controller with the added
+    // query param map to limit the result to just todos with
+    // body "tempor"
+    todoController.getTodos(ctx);
+
+    // Confirm that all the todos passed to `json` have body "tempor"
+    ArgumentCaptor<Todo[]> argument = ArgumentCaptor.forClass(Todo[].class);
+    verify(ctx).json(argument.capture());
+    for (Todo todo : argument.getValue()) {
+     assertTrue(todo.body.contains("tempor"));
+    }
+
+
+
+  }
+
+  @Test
+  public void respondsAppropriatelyToRequestForNonexistentId() throws IOException {
+    when(ctx.pathParam("id")).thenReturn(null);
+    Throwable exception = Assertions.assertThrows(NotFoundResponse.class, () -> {
+      todoController.getTodo(ctx);
+    });
+    assertEquals("No todo with id " + null + " was found.", exception.getMessage());
+  }
+
+  @Test
+  public void canGetTodosWithStatus() throws IOException {
+    Map<String, List<String>> queryParams = new HashMap<>();
+    queryParams.put("status", Arrays.asList(new String[] {"complete"}));
+    when(ctx.queryParamMap()).thenReturn(queryParams);
+
+    todoController.getTodos(ctx);
+
+    // Confirm that all the todos passed to `json` work for status "true".
+    ArgumentCaptor<Todo[]> argument = ArgumentCaptor.forClass(Todo[].class);
+    verify(ctx).json(argument.capture());
+    for (Todo todo : argument.getValue()) {
+      assertEquals(true, todo.status);
+    }
+  }
+
+  @Test
+  public void canGetTodosWithOrderByOwnerAndLimit() throws IOException {
+    Map<String, List<String>> queryParams = new HashMap<>();
+    queryParams.put("orderBy", Arrays.asList(new String[] {"owner"}));
+    queryParams.put("limit", Arrays.asList(new String[] {"3"}));
+    when(ctx.queryParamMap()).thenReturn(queryParams);
+
+    todoController.getTodos(ctx);
+
+    // Confirm that all the todos passed to `json` work for owner "Barry".
+    ArgumentCaptor<Todo[]> argument = ArgumentCaptor.forClass(Todo[].class);
+    verify(ctx).json(argument.capture());
+    for (Todo todo : argument.getValue()) {
+      System.out.println(todo.owner);
+      assertEquals("Barry", todo.owner);
+
+      // Barry should be the owner for first three alphabetically organized todos.
+      // This test shows that our limit filter works with our orderby Owner filter.
+    }
+  }
+  @Test
+  public void canGetTodosWithOrderByOwnerStatusCategory() throws IOException {
+    Map<String, List<String>> queryParams = new HashMap<>();
+    queryParams.put("orderBy", Arrays.asList(new String[] {"owner"}));
+    queryParams.put("status", Arrays.asList(new String[] {"complete"}));
+    queryParams.put("category", Arrays.asList(new String[] {"groceries"}));
+    queryParams.put("contains", Arrays.asList(new String[] {"Reprehenderit mollit "}));
+
+
+    when(ctx.queryParamMap()).thenReturn(queryParams);
+
+    todoController.getTodos(ctx);
+
+    // This test is to show that our code supports arbitrary combinations of filter.
+    // these filters should return the two todos that match all these conditions
+
+    ArgumentCaptor<Todo[]> argument = ArgumentCaptor.forClass(Todo[].class);
+    verify(ctx).json(argument.capture());
+    for (Todo todo : argument.getValue()) {
+
+      // both todos that are left after all the filters are applied are of category "groceries"
+
+      assertEquals("groceries", todo.category);
+
+      // both todos left after arbitrary filters have the same body which contains "mollit"
+
+      assertTrue(todo.body.contains("mollit"));
+
+
+
+}}}
